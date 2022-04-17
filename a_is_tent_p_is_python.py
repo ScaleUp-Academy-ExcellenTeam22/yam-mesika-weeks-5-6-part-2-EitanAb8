42469fc05e2198cec05824a8ffe9328ca4510a3a
EitanAb8 <88735044+EitanAb8@users.noreply.github.com>
2022-04-17 05:08:52 +0200

    Removed whitespaces

--- a_is_tent_p_is_python.py
+++ a_is_tent_p_is_python.py
@@ -3,8 +3,8 @@ def get_letters() -> list:
     The function creates an array of a-z and A-Z.
     :return: an array of letters.
     """
-    return [chr(letter) for letter in range(ord('a'), ord('z') + 1)] + \
-           [chr(letter) for letter in range(ord('A'), ord('Z') + 1)]
+    return [chr(letter) for letter in range(ord('a'), ord('z')+1)] +\
+           [chr(letter) for letter in range(ord('A'), ord('Z')+1)]
 
 
 def main():
